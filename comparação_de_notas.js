a1d940940846e3f986c30f5439a7b58413e40e2d
gabriel1325t <160737281+gabriel1325t@users.noreply.github.com>
2024-07-31 11:31:07 -0300

    Add files via upload

---
+++ comparação_de_notas.js
@@ -0,0 +1,11 @@
+const nota = 7
+
+if (nota 4 && 5){
+    console.log("nota "+nota+" recuperação")
+}else
+  if(nota >= 6){
+    console.log("nota"+nota" aprovado")
+  }else
+  if(nota < 4){
+    console.log("nota "+nota+"")
+  }
